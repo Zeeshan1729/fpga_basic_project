cb16c9c90ff05184c65a68aa088b0ed852ead3a2
Zeeshan Mehraj <meerzeeshan173@gmail.com>
2026-05-15 12:13:26 +0530

    Implement seven segment display logic

--- simple_alu/rtl/seven_seg.v
+++ simple_alu/rtl/seven_seg.v
@@ -1,1 +1,36 @@
 
+module seven_seg(
+
+    input [3:0] value,
+    output reg [6:0] seg
+
+);
+
+always @(*)
+begin
+
+    case(value)
+
+        // Numbers
+        4'd0: seg = 7'b1000000;
+        4'd1: seg = 7'b1111001;
+        4'd2: seg = 7'b0100100;
+        4'd3: seg = 7'b0110000;
+        4'd4: seg = 7'b0011001;
+        4'd5: seg = 7'b0010010;
+        4'd6: seg = 7'b0000010;
+        4'd7: seg = 7'b1111000;
+
+        // Minus sign
+        4'd10: seg = 7'b0111111;
+
+        // Blank
+        4'd15: seg = 7'b1111111;
+
+        default: seg = 7'b1111111;
+
+    endcase
+
+end
+
+endmodule
