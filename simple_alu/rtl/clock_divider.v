141388dd7ebd76a6775a043dc92f50e32e8e4643
Zeeshan Mehraj <meerzeeshan173@gmail.com>
2026-05-15 12:11:41 +0530

    Add clock divider module with counter and output

--- simple_alu/rtl/clock_divider.v
+++ simple_alu/rtl/clock_divider.v
@@ -1,1 +1,18 @@
 
+module clock_divider(
+
+    input clk,
+    output [2:0] scan
+
+);
+
+reg [19:0] counter;
+
+always @(posedge clk)
+begin
+    counter <= counter + 1;
+end
+
+assign scan = counter[19:17];
+
+endmodule
